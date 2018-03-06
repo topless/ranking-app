20afb02911c8517e31df01100be356f0573578d9
Christos Topaloudis <topless@gmail.com>
2018-03-06 13:54:28 +0100

    Theming

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,7 +10,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+  @import "~vue-material/dist/theme/engine";
+  @include md-register-theme("default", (
+    theme: dark
+  ));
+  @import "~vue-material/dist/theme/all";
+
 #app {
   padding-top: 128px;
 }
